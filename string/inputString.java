b551cc17cf011f5a47e887610048daad064629d2
nasir-buddy <syedali172000@gmail.com>
2024-05-20 01:33:18 +0530

    String: string input

---
+++ string/inputString.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.util.Scanner;
+
+public class inputString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter you name");
+        String str = sc.nextLine();
+        System.out.println("Printing the string : " + str);
+    }
+}
