1e7ee8e03cd6cc475727c63530631c59808b5007
nasir-buddy <syedali172000@gmail.com>
2024-05-25 19:47:21 +0530

    OOPS2: Inheritance

---
+++ OOPS2/Car.java
@@ -0,0 +1,10 @@
+package OOPS2;
+
+public class Car extends Vehicle{
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.wheel = 12;
+        car1.start();
+    }
+    
+}
---
+++ OOPS2/Vehicle.java
@@ -0,0 +1,10 @@
+package OOPS2;
+
+class Vehicle {
+    int wheel;
+
+    void start(){
+        System.out.println("Car is starting.");
+    }
+     
+}
