ae796cc222a81609c832ad0fedb331dd180ed3c7
nasir-buddy <syedali172000@gmail.com>
2024-05-28 05:47:44 +0530

    OOPS4: Inner class

---
+++ OOPS4/InnerClass.java
@@ -0,0 +1,11 @@
+package OOPS4;
+
+public class InnerClass {
+    class Toy {
+        int price;
+    }
+    public static void main(String[] args) {
+        Toy t1 = new InnerClass(). new Toy();
+
+    }
+}
