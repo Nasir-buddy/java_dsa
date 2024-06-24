936527969624fb53812780994cf1630756d3d054
nasir-buddy <syedali172000@gmail.com>
2024-06-25 02:02:13 +0530

    Generic method without static

--- generic/generic_method.java
+++ generic/generic_method.java
@@ -5,8 +5,16 @@ public class generic_method {
        printData("Hello");
        printData(234);
        printData(true);
+
+        generic_method obj = new generic_method();
+        obj.Data(345);
+        obj.Data("Nasir Ali");
     }
     static <G> void printData(G data){
+        System.out.println(data);
+    }
+
+    <V> void Data(V data){
         System.out.println(data);
     }
 }
