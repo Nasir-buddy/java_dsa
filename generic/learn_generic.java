e91571865b1f2e5090cd452dbb8c252c0b0934d6
nasir-buddy <syedali172000@gmail.com>
2024-06-25 01:41:52 +0530

    oops: generic show

--- generic/learn_generic.java
+++ generic/learn_generic.java
@@ -4,13 +4,16 @@ public class learn_generic {
     public static void main(String[] args) {
         Animal<String> a1 = new Animal<>("Dog");
         Animal<Integer> a2 = new Animal<>(34);
+        System.out.println(a1.getID());
     }
 }
 class Animal<G>{
     G name;
 
     public Animal(G name){
-        this.name = name;
-        
+        this.name = name;       
+    }
+    G getID(){
+        return name;
     }
 }
